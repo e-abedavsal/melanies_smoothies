096594abca23bc39b2610f49b71c6de2a4ea1246
e-abedavsal <abel.david.salazar@ericsson.com>
2024-05-15 15:54:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,10 @@ session = cnx.session()
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your smoothie will be', name_on_order)
 
+pd_df=my_dataframe.to_pandas()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 ingredients_list= st.multiselect('choose up to 5 ingredients', my_dataframe, max_selections=5)
 
