d212f4f90373f58bae00e1cd26b7f800f04e1ef7
e-abedavsal <abel.david.salazar@ericsson.com>
2024-05-15 15:57:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.context import get_active_session
 import requests
+import pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
